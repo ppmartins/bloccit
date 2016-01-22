8323e2cee8d3c3f2dd134e0ed212e7943c87b299
Pedro <ppereiramartins@gmail.com>
2016-01-22 15:28:14 +0000

    Checkpoint 30 seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
+include RandomData
+
+50.times do
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
