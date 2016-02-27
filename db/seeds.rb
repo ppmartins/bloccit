df0dbd8a0546e573ce1a27edba3ada85bff02c0a
Pedro <ppereiramartins@gmail.com>
2016-02-27 15:11:01 +0000

    Checkpoint 39 work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -25,7 +26,25 @@ posts = Post.all
   )
 end
 
+
+5.times do
+  User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
+user = User.first
+user.update_attributes!(
+  email: "ppereiramartins@gmail.com",
+  password: "helloworld"
+)
+
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
