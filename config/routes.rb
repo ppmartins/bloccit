b8307d88ed926c29fa4b98c3e78f8b72c9d397da
Pedro <ppereiramartins@gmail.com>
2016-03-01 16:57:51 +0000

    Checkpoint 42 labels completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :labels, only: [:show]
+
   resources :topics do
     resources :posts, except: [:index]
   end
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
