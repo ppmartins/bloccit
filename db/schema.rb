e6d16b69fa53b18ecb7a11b4203bfd3c861fb1c9
Pedro <ppereiramartins@gmail.com>
2016-03-02 14:54:58 +0000

    Checkpoint 43 voting completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301143339) do
+ActiveRecord::Schema.define(version: 20160302135931) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160301143339) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -85,8 +86,10 @@ ActiveRecord::Schema.define(version: 20160301143339) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "topic_id"
+    t.integer  "comments_id"
   end
 
+  add_index "topics", ["comments_id"], name: "index_topics_on_comments_id"
   add_index "topics", ["topic_id"], name: "index_topics_on_topic_id"
 
   create_table "users", force: :cascade do |t|
@@ -97,5 +100,16 @@ ActiveRecord::Schema.define(version: 20160301143339) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
