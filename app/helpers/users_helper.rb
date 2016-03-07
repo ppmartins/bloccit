4950d69f342f213d29a48b7e3b355afbcc86fedb
Pedro <ppereiramartins@gmail.com>
2016-03-07 09:23:16 +0000

    Assignment 45 work in progress 1

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def user_has_posts?(posts)
+    if posts.count == 0
+      false
+    else
+      true
+    end
+  end
+
+  def user_has_comments?(comments)
+    if comments.count == 0
+      false
+    else
+      true
+    end
+  end
 end
